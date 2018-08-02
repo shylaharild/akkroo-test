8ccdbd3fe94c18ccf96fea8cbe7f1a5b5067b336
Shyla Hari <shyla.hari@polestarglobal.com>
2018-08-02 16:05:30 +0100

    manifest to run the website module for website02

---
+++ puppet-base/manifests/website02.pp
@@ -0,0 +1,16 @@
+# Website 02 service
+
+Class['base'] ~> Class['website']
+
+class { 'base':
+  host_name               => $host_name,
+  fqdn                    => $fqdn,
+  provider                => $provider,
+  service                 => $service,
+  vagrant_user            => $vagrant_user,
+  before                  => Class['website'],
+}
+
+class { 'website':
+  service => $service,
+}
